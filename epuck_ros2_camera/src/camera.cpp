252bb66c84cc9f147fa7a8b594a31c0cc15bab13
Darko Lukic <lukicdarkoo@gmail.com>
2020-03-02 11:43:02 +0100

    Add parameters

--- epuck_ros2_camera/src/camera.cpp
+++ epuck_ros2_camera/src/camera.cpp
@@ -5,7 +5,8 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/string.hpp>
 
-extern "C" {
+extern "C"
+{
 #include "epuck_ros2_camera/pipuck_image.h"
 #include "epuck_ros2_camera/pipuck_v4l2.h"
 #include "epuck_ros2_camera/pipuck_jpeg.h"
@@ -14,56 +15,119 @@ extern "C" {
 
 using namespace std::chrono_literals;
 
-class CameraPublisher : public rclcpp::Node {
+class CameraPublisher : public rclcpp::Node
+{
 public:
-  CameraPublisher() : Node("camera_publisher"), count(0) {
+  CameraPublisher() : Node("camera_publisher"), v4l2_initialized(false), jpeg_initialized(false)
+  {
+    auto quality = declare_parameter<int>("quality", 8);
+    auto interval = declare_parameter<int>("interval", 80);
+
     pipuck_image_init(&captured_image);
     pipuck_image_init(&compressed_image);
 
-    compressed_image.quality = 10;
+    compressed_image.quality = quality;
     compressed_image.data = compressed_image_buffer;
     captured_image.encoding = PIPUCK_IMAGE_ENCODING_YUYV;
 
     pipuck_ov7670_init();
-    pipuck_v4l2_init();
-    pipuck_jpeg_init(&captured_image, &compressed_image);
 
-    publisher = this->create_publisher<sensor_msgs::msg::CompressedImage>(
-        "image_raw/compressed", 1);
+    callback_handler = this->add_on_set_parameters_callback(std::bind(&CameraPublisher::param_change_callback, this, std::placeholders::_1));
+
+    publisher_compressed = this->create_publisher<sensor_msgs::msg::CompressedImage>(
+        "image_raw/compressed", 0);
+    publisher_raw = this->create_publisher<sensor_msgs::msg::Image>(
+        "image_raw", 0);
     timer = this->create_wall_timer(
-        100ms, std::bind(&CameraPublisher::timer_callback, this));
+        std::chrono::milliseconds(interval), std::bind(&CameraPublisher::timer_callback, this));
   }
 
-  ~CameraPublisher() {
+  ~CameraPublisher()
+  {
     pipuck_v4l2_deinit();
     pipuck_jpeg_deinit();
   }
 
 private:
-  void timer_callback() {
-    pipuck_v4l2_capture(&captured_image);
-    pipuck_jpeg_encode(&captured_image, &compressed_image);
+  rcl_interfaces::msg::SetParametersResult param_change_callback(std::vector<rclcpp::Parameter> parameters)
+  {
+    auto result = rcl_interfaces::msg::SetParametersResult();
+    result.successful = true;
 
-    auto message = sensor_msgs::msg::CompressedImage();
-    message.format = "jpeg";
-    message.header.stamp = now();
-    message.header.frame_id = "pipuck_image";
-    message.data.assign(compressed_image.data,
-                        compressed_image.data + compressed_image.size);
+    for (auto parameter : parameters)
+    {
+      if (parameter.get_name() == "quality")
+      {
+        pipuck_jpeg_deinit();
+        compressed_image.quality = parameter.as_int();
+        pipuck_jpeg_init(&captured_image, &compressed_image);
+      }
+      else if (parameter.get_name() == "interval")
+      {
+        timer->cancel();
+        timer = this->create_wall_timer(
+            std::chrono::milliseconds(parameter.as_int()), std::bind(&CameraPublisher::timer_callback, this));
+      }
 
-    RCLCPP_INFO(this->get_logger(), "Publishing: %d", count);
-    publisher->publish(message);
-    count++;
+      RCLCPP_INFO(
+          this->get_logger(),
+          "Parameter '%s' has changed to %s",
+          parameter.get_name().c_str(),
+          parameter.value_to_string().c_str());
+    }
+
+    return result;
+  }
+
+  void timer_callback()
+  {
+    if (publisher_compressed->get_subscription_count() > 0 || publisher_raw->get_subscription_count())
+    {
+      if (v4l2_initialized == false)
+      {
+        pipuck_v4l2_init();
+        v4l2_initialized = true;
+      }
+      pipuck_v4l2_capture(&captured_image);
+    }
+
+    if (publisher_raw->get_subscription_count() > 0)
+    {
+      // TODO: Publish raw
+    }
+
+    if (publisher_compressed->get_subscription_count() > 0)
+    {
+      if (jpeg_initialized == false)
+      {
+        pipuck_jpeg_init(&captured_image, &compressed_image);
+        jpeg_initialized = true;
+      }
+      pipuck_jpeg_encode(&captured_image, &compressed_image);
+
+      auto message = sensor_msgs::msg::CompressedImage();
+      message.format = "jpeg";
+      message.header.stamp = now();
+      message.header.frame_id = "pipuck_image";
+      message.data.assign(compressed_image.data,
+                          compressed_image.data + compressed_image.size);
+
+      publisher_compressed->publish(message);
+    }
   }
   rclcpp::TimerBase::SharedPtr timer;
-  rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr publisher;
-  size_t count;
+  rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr publisher_compressed;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_raw;
   pipuck_image_t captured_image;
   pipuck_image_t compressed_image;
+  OnSetParametersCallbackHandle::SharedPtr callback_handler;
   char compressed_image_buffer[500 * 1024];
+  bool v4l2_initialized;
+  bool jpeg_initialized;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<CameraPublisher>());
   rclcpp::shutdown();
