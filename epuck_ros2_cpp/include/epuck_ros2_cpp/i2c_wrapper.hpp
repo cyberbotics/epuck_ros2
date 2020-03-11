1aef1e75577aa52e8825000dd873ac467926fd12
Darko Lukic <lukicdarkoo@gmail.com>
2020-03-11 17:42:57 +0100

    Add foundation for epuck_ros2_cpp tests

---
+++ epuck_ros2_cpp/include/epuck_ros2_cpp/i2c_wrapper.hpp
@@ -0,0 +1,105 @@
+#ifndef I2C_WRAPPER_H
+#define I2C_WRAPPER_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+extern "C"
+{
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+}
+
+class I2CWrapper
+{
+public:
+    virtual int set_address(int address) = 0;
+    virtual int read_data(char *buffer, int size) = 0;
+    virtual int write_data(char *buffer, int size) = 0;
+};
+
+class I2CWrapperTest : public I2CWrapper
+{
+public:
+    I2CWrapperTest(){};
+    I2CWrapperTest(std::string device)
+    {
+        base_filename = "/tmp" + device;
+
+        // Create folder
+        std::string folder = base_filename.substr(0, base_filename.find_last_of("/"));
+        mkdir(folder.c_str(), 0777);
+        std::cout << "Folder " << folder << " is created" << std::endl;
+    };
+
+    int set_address(int address)
+    {
+        std::fstream stream(base_filename + "_ioctl", std::ios::out | std::ios::binary);
+        stream << address << std::endl;
+        stream.close();
+    };
+
+    int read_data(char *buffer, int size)
+    {
+        std::fstream stream(base_filename + "_read", std::ios::in | std::ios::binary);
+        stream.write(buffer, size);
+        stream.close();
+        return size;
+    };
+
+    int write_data(char *buffer, int size)
+    {
+        std::cout << "Writting data..." << base_filename + "_write" << std::endl;
+        std::cout.write(buffer, size);
+
+        std::fstream stream;
+        stream.open(base_filename + "_write", std::ios::out | std::ios::binary);
+
+        stream.write(buffer, size);
+        stream.close();
+        return size;
+    };
+
+private:
+    std::string base_filename;
+};
+
+class I2CWrapperHW : public I2CWrapper
+{
+public:
+    I2CWrapperHW(){};
+
+    I2CWrapperHW(std::string device)
+    {
+        fh = open(device.c_str(), O_RDWR);
+        std::cout << fh << std::endl;
+        if (fh < 0)
+        {
+            std::cout << "Cannot open file: " << device << std::endl;
+        }
+    };
+
+    int set_address(int address)
+    {
+        return ioctl(fh, I2C_SLAVE, address);
+    };
+
+    int read_data(char *buffer, int size)
+    {
+        return read(fh, buffer, size);
+    };
+
+    int write_data(char *buffer, int size)
+    {
+        return write(fh, buffer, size);
+    };
+
+private:
+    int fh;
+};
+
+#endif
