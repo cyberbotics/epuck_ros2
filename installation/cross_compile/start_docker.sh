2d0fee19d1c231c1429c6b60bc872d7ca9963182
Darko Lukic <lukicdarkoo@gmail.com>
2020-07-13 15:20:15 +0200

    rootfs should be writtable in container

--- installation/cross_compile/start_docker.sh
+++ installation/cross_compile/start_docker.sh
@@ -2,10 +2,12 @@
 
 if [ ! -d "./ros2_ws" ]; then
   mkdir -p ./ros2_ws
+  chmod 777 ./ros2_ws
 fi
 
 if [ ! -d "./rootfs" ]; then
   mkdir -p ./rootfs
+  chmod 777 ./rootfs
 fi
 
 echo "Building docker image..."
